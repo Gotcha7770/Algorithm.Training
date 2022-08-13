d36d2f9e6d8398fac013274872b0dd89e0029422
Gotcha7770 <gbakanov@inbox.ru>
2022-08-13 13:51:47 +0300

    count pages

---
+++ CS.Algorithm.Training/Task5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace Algorithm.Training
+{
+    public class Task5
+    {
+        public int CountPages(int total, int pageSize)
+        {
+            //return total / total + (total % pageSize > 0 ? 1 : 0);
+            //return (total + pageSize - 1) / pageSize;
+            return (total - 1) / pageSize + 1;
+        }
+        
+        [Theory]
+        [InlineData(2, 3, 1)]
+        [InlineData(3, 3, 1)]
+        [InlineData(4, 3, 2)]
+        [InlineData(5, 3, 2)]
+        [InlineData(6, 3, 2)]
+        [InlineData(7, 3, 3)]
+        public void Acceptance(int total, int pageSize, int expected)
+        {
+            CountPages(total, pageSize).ShouldBe(expected);
+        }
+    }
+}
