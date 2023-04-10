dee11c81843fb5ac10b33b8d74048f62ae8b4034
Gotcha7770 <gbakanov@inbox.ru>
2023-04-10 10:15:32 +0300

    Task 7

---
+++ CS.Algorithm.Training/Task7.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithm.Training;
+
+public class Task7
+{
+    // есть строка, состоит из предложений разделенных разными знаками типа точки, вопроса и т. д.
+    // Все в нижнем регистре, надо первую букву каждого предложения перевести в верхний
+
+    private readonly char[] _delimiters = { '.', '!', '?' };
+    private readonly string _example = "тестовый текст. тестовый, тестовый текст?  тестовый! тест тест тест.";
+
+    [Fact]
+    public void Test()
+    {
+        string[] result = Divide(_example);
+
+        result.Should().BeEquivalentTo(new []
+        {
+            "Тестовый текст.",
+            "Тестовый, тестовый текст?",
+            "Тестовый!",
+            "Тест тест тест."
+        });
+    }
+
+    private string[] Divide(string input)
+    {
+        throw new System.NotImplementedException();
+    }
+}
