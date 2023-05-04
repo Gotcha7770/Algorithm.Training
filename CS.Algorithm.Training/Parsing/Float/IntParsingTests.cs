0edfb4765981a7876f1802982d4743db9de099ad
Gotcha7770 <gbakanov.box@gmail.com>
2023-05-04 19:51:56 +0300

    added processing of invalid characters

--- CS.Algorithm.Training/Parsing/Float/IntParsingTests.cs
+++ CS.Algorithm.Training/Parsing/Float/IntParsingTests.cs
@@ -1,11 +1,42 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
+using static FluentAssertions.FluentActions;
 
 namespace Algorithm.Training.Parsing.Float;
 
 public class IntParsingTests
 {
+    [Fact]
+    public void Custom_Null_Throws()
+    {
+        Invoking(() => ParseInt(null))
+            .Should()
+            .Throw<ArgumentNullException>();
+    }
+    
+    [Fact]
+    public void Custom_EmptyString_Throws()
+    {
+        Invoking(() => ParseInt(string.Empty))
+            .Should()
+            .Throw<FormatException>()
+            .WithMessage("string length is 0");
+    }
+    
+    [Theory]
+    [InlineData("a", "invalid character at 0 index")]
+    [InlineData("12a4", "invalid character at 2 index")]
+    [InlineData("-12a4", "invalid character at 2 index")]
+    public void Custom_InvalidCharacters_Throws(string input, string message)
+    {
+        Invoking(() => ParseInt(input))
+            .Should()
+            .Throw<FormatException>()
+            .WithMessage(message);
+    }
+
     [Theory]
     [InlineData("0", 0)]
     [InlineData("1", 1)]
@@ -24,7 +55,7 @@ public class IntParsingTests
     [InlineData("10", 10)]
     [InlineData("1234", 1234)]
     [InlineData("-1234", -1234)]
-    public void TryParse(string input, int output)
+    public void SystemParse(string input, int output)
     {
         int result = int.Parse(input);
         result.Should().Be(output);
@@ -32,20 +63,41 @@ public class IntParsingTests
 
     private static int ParseInt(string input)
     {
-        var result = 0;
-        foreach (char character in input)
+        if (input is null)
+            throw new ArgumentNullException(nameof(input));
+
+        if (input.Length == 0)
+            throw new FormatException("string length is 0");
+
+        return input[0] == '-' 
+            ? -1 * ParseIntWithoutSign(input.AsSpan(1))
+            : ParseIntWithoutSign(input);
+            
+    }
+
+    private static int ParseIntWithoutSign(string input)
+    {
+        return input
+            .Select((x, i) => IsCharInvalid(x) ? throw new FormatException($"invalid character at {i} index"): x)
+            .Aggregate(0, (acc, current) => acc * 10 + CharToInt(current));
+    }
+    
+    private static int ParseIntWithoutSign(ReadOnlySpan<char> input)
+    {
+        int result = 0;
+        for (int i = 0; i < input.Length; i++)
         {
+            char character = input[i];
+            if (IsCharInvalid(character))
+                throw new FormatException($"invalid character at {i} index");
             result *= 10;
             result += CharToInt(character);
         }
 
         return result;
     }
-
-    private static int ParseIntWithoutSign(string input)
-    {
-        return input.Aggregate(0, (acc, current) => acc * 10 + current);
-    }
     
+    private static bool IsCharInvalid(char x) => x is < '0' or > '9';
+
     private static int CharToInt(char x) => x - '0';
 }
