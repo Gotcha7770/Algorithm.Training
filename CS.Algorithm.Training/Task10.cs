04ab34ce5f45e9880f29b3a8bf7c47bba0247860
Gotcha7770 <gbakanov.box@gmail.com>
2023-11-30 13:29:27 +0300

    more variants

--- CS.Algorithm.Training/Task10.cs
+++ CS.Algorithm.Training/Task10.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
 
@@ -13,7 +14,13 @@ namespace Algorithm.Training;
 
 public class Task10
 {
-    private int GetMaxOnes(int[] source)
+    /// <summary>
+    /// Gets the maximum number of consecutive 1s in the input array.
+    /// Single 0s within the sequence of 1s are treated as part of the sequence.
+    /// </summary>
+    /// <param name="source">The input array of integers.</param>
+    /// <returns>The maximum number of consecutive 1s considering single interspersed 0s as part of the sequence.</returns>
+    private int GetMaxConsecutiveOnes(int[] source)
     {
         int max = 0;
         int current = 0;
@@ -23,6 +30,7 @@ public class Task10
             if (i == 1)
             {
                 current++;
+                isGap = false;
             }
             else if (!isGap)
             {
@@ -39,15 +47,131 @@ public class Task10
         return max;
     }
 
+    private int GetMaxConsecutiveOnesFromAI(int[] inputArray)
+    {
+        int maxConsecutiveOnes = 0;
+        int currentConsecutiveOnes = 0;
+        int zeroCount = 0;
+        int previousCount = 0;
+
+        foreach (var element in inputArray)
+        {
+            if (element == 1)
+            {
+                currentConsecutiveOnes++;
+                maxConsecutiveOnes = Math.Max(maxConsecutiveOnes, currentConsecutiveOnes + previousCount);
+            }
+            else // element == 0
+            {
+                zeroCount++;
+                if (zeroCount == 2)
+                {
+                    zeroCount = 1;
+                    previousCount = 0;
+                }
+                else
+                {
+                    previousCount = currentConsecutiveOnes;
+                    currentConsecutiveOnes = 0;
+                }
+            }
+        }
+        
+        return maxConsecutiveOnes;
+    }
+
+    private int GetMaxConsecutiveOnes2(IEnumerable<int> source)
+    {
+        int result = 0;
+        using var enumerator = source.GetEnumerator();
+
+        // 1 - сумма
+        // 0 - умножение
+        
+        // 10
+        // 01
+        // 00
+        
+        // current > previous
+        // 3 > 2
+
+        if (!enumerator.MoveNext())
+            return 0;
+
+        int consecutiveOnesCount = enumerator.Current;
+        int prev = enumerator.Current;
+        while (enumerator.MoveNext())
+        {
+            consecutiveOnesCount += enumerator.Current;
+
+            if (enumerator.Current + prev < 1)
+            {
+                result = Math.Max(result, consecutiveOnesCount);
+                consecutiveOnesCount = 0;
+            }
+            
+            prev = enumerator.Current;
+        }
+
+        return Math.Max(result, consecutiveOnesCount);
+    }
+
+    private int GetMaxConsecutiveOnes3(int[] source)
+    {
+        int result = 0;
+        int consecutiveOnesCount = source.Length > 0 ? source[0] : 0;
+        for (int i = 1; i < source.Length; i++)
+        {
+            consecutiveOnesCount += source[i];
+
+            if (source[i] + source[i - 1] < 1)
+            {
+                result = Math.Max(result, consecutiveOnesCount);
+                consecutiveOnesCount = 0;
+            }
+        }
+        
+        return Math.Max(result, consecutiveOnesCount);
+    }
+    
+    int GetMaxConsecutiveOnes4(int[] seq)
+    {
+        var maxSum = 0;
+        var prevSeqSize = 0;
+        var curSeqSize = 0;
+        var seqCount = 0;
+        var sumLastTwo =0;
+        for (var i = 0; i < seq.Length; i++){
+            if (seq[i] == 1) ++curSeqSize;
+            if (seq[i] == 0)
+            {
+                sumLastTwo = prevSeqSize + curSeqSize;
+                if (prevSeqSize + curSeqSize > maxSum) maxSum = sumLastTwo;
+                prevSeqSize = curSeqSize;
+                ++seqCount;
+                curSeqSize = 0;
+            }
+        }
+        sumLastTwo = prevSeqSize + curSeqSize;
+        if (sumLastTwo > maxSum) maxSum = sumLastTwo;
+        if (seqCount == 0) --maxSum;
+        return maxSum;
+    }
+
     [Theory]
     [InlineData(new int[0], 0)]
     [InlineData(new[] { 0 }, 0)]
+    [InlineData(new[] { 1 }, 1)]
     [InlineData(new[] { 0, 1 }, 1)]
     [InlineData(new[] { 1, 1, 0, 1 }, 3)]
     [InlineData(new[] { 1, 1, 0, 0, 1 }, 2)]
+    [InlineData(new[] { 1, 1, 0, 1, 1, 0 }, 4)]
+    [InlineData(new[] { 0, 0, 0, 0, 0 }, 0)]
+    [InlineData(new[] { 1, 1, 1, 1, 1 }, 5)]
+    [InlineData(new[] { 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1 }, 7)]
     public void Acceptance(int[] items, int expected)
     {
-        GetMaxOnes(items)
+        GetMaxConsecutiveOnes3(items)
             .Should()
             .Be(expected);
     }
