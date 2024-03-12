a37bb868ff12e0123d30d7391a3190285e7077f8
Gotcha7770 <gbakanov.box@gmail.com>
2024-03-13 00:54:22 +0300

    example of async method invocation in computation expression

---
+++ FS.Algorithm.Training/Task12.fs
@@ -0,0 +1,20 @@
+﻿module FS.Algorithm.Training.Task12
+
+open FSharp.Control
+
+let urls = ["test1.com/items"; "test2.com/items"]
+let httpClient = new System.Net.Http.HttpClient()
+
+let seqOfTasks = seq {
+    for url in urls do
+        httpClient.GetAsync(url)
+}
+
+let handle url response =
+    "All Ok"
+
+let seq = asyncSeq {
+    for url in urls do
+        let! response = httpClient.GetAsync(url) |> Async.AwaitTask
+        if response.IsSuccessStatusCode then yield handle url response
+    }
