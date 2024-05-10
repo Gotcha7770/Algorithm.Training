45467a948c23ce63dcf1a9dfceb2003278ac160d
Gotcha7770 <gbakanov.box@gmail.com>
2024-05-10 18:43:53 +0300

    xml parser

---
+++ CS.Algorithm.Training/Parsing/XML/XmlParser.cs
@@ -0,0 +1,19 @@
+﻿namespace Algorithm.Training.Parsing.XML;
+
+public class XmlNode
+{
+    
+}
+
+public class XmlParser
+{
+    // public static XmlNode Load(string path)
+    // {
+    //     var parser = new XmlParser(path);
+    // }
+
+    private XmlParser(string path)
+    {
+        
+    }
+}
---
+++ CS.Algorithm.Training/Parsing/XML/XmlParsingTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Xml;
+using System.Xml.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithm.Training.Parsing.XML;
+
+public class XmlParsingTests
+{
+    private const string PathToXml = "Algorithm.Training.Parsing.XML.example.xml";
+
+    [Fact]
+    public void XmlDocument()
+    {
+        var xDocument = new XmlDocument();
+        using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(PathToXml))
+        {
+            xDocument.Load(stream);
+        }
+
+        xDocument.Should().NotBeNull();
+        xDocument.DocumentElement.Name.Should().Be("Document");
+    }
+
+    [Fact]
+    public void LinqToXml()
+    {
+        XDocument xDocument;
+        using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(PathToXml))
+        {
+            xDocument = XDocument.Load(stream);
+        }
+
+        xDocument.Should()
+            .NotBeNull()
+            .And.HaveRoot("Document")
+            .And.HaveElement("Property");
+    }
+}
