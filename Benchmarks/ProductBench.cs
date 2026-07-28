11dd1582d871f2aa7747c1430fe562b9de14a071
Gotcha7770 <gbakanov.box@gmail.com>
2026-07-28 14:22:06 +0300

    F# cartesian product + benchmarks

---
+++ Benchmarks/ProductBench.cs
@@ -0,0 +1,76 @@
+﻿using Algorithm.Training;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using Microsoft.FSharp.Collections;
+
+namespace Benchmarks;
+
+[MemoryDiagnoser]
+public class ProductBench
+{
+    public static IEnumerable<ProductCase> Cases =>
+    [
+        // фиксированная глубина
+        new(2, 5),
+        new(5, 5),
+        new(10, 5),
+
+        // фиксированная ширина
+        new(2, 10),
+        new(2, 15),
+        new(2, 20),
+    ];
+
+    [ParamsSource(nameof(Cases))] 
+    public ProductCase Case { get; set; }
+
+    private IEnumerable<IEnumerable<int>> Items =>
+        Enumerable.Range(0, Case.Depth)
+            .Select(_ => Enumerable.Range(1, Case.Width));
+
+    private FSharpList<IEnumerable<int>> ItemsList => ListModule.OfSeq(Items);
+
+    private readonly Consumer _consumer = new();
+
+    [Benchmark(Baseline = true)]
+    public void CSharp_Product()
+    {
+        Items
+            .Product()
+            .Select(x => x.ToArray())
+            .Consume(_consumer);
+    }
+    
+    [Benchmark]
+    public void FSharp_Product1()
+    {
+        FS.Algorithm.Training.Task18.product1(Items)
+            .Select(x => x.ToArray())
+            .Consume(_consumer);
+    }
+
+    [Benchmark]
+    public void FSharp_Product2()
+    {
+        FS.Algorithm.Training.Task18.product2(Items)
+            .Select(x => x.ToArray())
+            .Consume(_consumer);
+    }
+
+    [Benchmark]
+    public void FSharp_Product3()
+    {
+        FS.Algorithm.Training.Task18.product3(ItemsList)
+            .Consume(_consumer);
+    }
+}
+
+public record ProductCase(int Width, int Depth)
+{
+    public override string ToString()
+    {
+        return Width == 2
+            ? $"2^{Depth}"
+            : string.Join("×", Enumerable.Repeat(Width, Depth));
+    }
+}
---
+++ FS.Algorithm.Training/Task18.fs
@@ -0,0 +1,79 @@
+﻿module FS.Algorithm.Training.Task18
+
+open Xunit
+open Swensen.Unquote
+
+// calque from C# LINQ implementation
+let product1 (input: seq<seq<'a>>) =
+    (seq { Seq.empty }, input)
+    ||> Seq.fold (fun acc cur ->
+        seq {
+            for prefix in acc do
+                for item in cur do
+                    yield
+                        seq {
+                            yield! prefix
+                            yield item
+                        }
+        })
+
+// optimized using lists
+let product2 (input: seq<seq<'a>>) =
+    (seq { [] }, input)
+    ||> Seq.fold (fun acc cur ->
+        seq {
+            for prefix in acc do
+                for item in cur do
+                    yield item :: prefix
+        })
+    |> Seq.map List.rev
+
+// recursive using lists
+let rec product3 (input: list<seq<'a>>) =
+    match input with
+    | [] -> [ [] ]
+    | head :: tail ->
+        let suffixes = product3 tail
+        [ for item in head do
+              for rest in suffixes do
+                  yield item :: rest ]
+
+// let productWithCondition (condition: 'a -> 'a -> bool) (input: seq<seq<'a>>) : 'a list list =
+//     ([[]], input)
+//     ||> Seq.fold (fun acc cur ->
+//         acc
+//         |> List.collect (fun prev ->
+//             cur
+//             // |> Seq.filter (fun item -> match List.tryLast prev with
+//             //                             | Some x -> condition x item
+//             //                             | None -> true)
+//             |> Seq.map (fun item -> prev @ item))
+//         )
+
+
+type ProductCases() as this =
+    inherit TheoryData<int list list, int list list>()
+    do this.Add([ [ 1; 2 ]; [ 3; 4 ] ], [ [ 1; 3 ]; [ 1; 4 ]; [ 2; 3 ]; [ 2; 4 ] ])
+
+[<Fact>]
+let ``seq equivalence`` () =
+    test
+        <@
+            seq {
+                1
+                2
+                3
+            } = seq {
+                1
+                2
+                3
+            }
+        @>
+
+[<Fact>]
+let ``built-in cartesian product only for 2 sequences`` () =
+    test <@ ([ 1; 2 ], [ 3; 4 ]) ||> Seq.allPairs |> Seq.toList = [ (1, 3); (1, 4); (2, 3); (2, 4) ] @>
+
+[<Theory>]
+[<ClassData(typeof<ProductCases>)>]
+let ``get cartesian product`` input expected = test <@ product3  (List.ofSeq input) = expected @>
